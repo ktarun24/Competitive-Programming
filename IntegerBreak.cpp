9ecc83be227a071c4b1ae137d0388accaca23c2c
skglt <54747258+skglt@users.noreply.github.com>
2020-10-01 15:38:17 +0530

    Update IntegerBreak.cpp

--- IntegerBreak.cpp
+++ IntegerBreak.cpp
@@ -1,5 +1,5 @@
 //Given a positive integer n, break it into the sum of at least two positive integers 
-//and maximize the product of those integers. Return the maximum product you can get.
+//and maximize the product of those integers. Return the maximum product you can get .
 
 //Question link : https://leetcode.com/problems/integer-break/
 
